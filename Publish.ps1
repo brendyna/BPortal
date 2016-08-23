b2a7421f64cc1a3a77222d5a472128c7bdb938b7
brendyna <brendyna@microsoft.com>
2016-08-22 20:24:17 -0700

    Minifying output on build for package publishing

--- Publish.ps1
+++ Publish.ps1
@@ -2,4 +2,4 @@ Write-Host "Publishing packages"
 new-item .\.nuget\packages\obj -type directory -force | out-null
 
 gci .\.nuget\packages\obj\*.nupkg -File |
-    foreach { nuget push $_.FullName -Source https://microsoft.pkgs.visualstudio.com/DefaultCollection/_packaging/WPT.WPS/nuget/v2 -ApiKey VSTS }
+    foreach { nuget push $_.FullName -Source https://microsoft.pkgs.visualstudio.com/DefaultCollection/_packaging/WPT.WPS/nuget/v3/index.json -ApiKey WPT.WPS }
