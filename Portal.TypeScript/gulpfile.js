49943e9b6dbb8a444df60cd2d3948bc787c91753
brendyna <brendyna@microsoft.com>
2016-08-26 01:29:00 -0700

    Adding Node and Gulp configurations to trigger Bower package installation before build

---
+++ Portal.TypeScript/gulpfile.js
@@ -0,0 +1,29 @@
+﻿var gulp = require('gulp'),
+    bower = require('gulp-bower'),
+    $ = require('gulp-load-plugins')({
+        pattern: [
+        'del'
+        ]
+    });
+
+// Path settings for Gulp
+var config = {
+    bowerInstallPaths: [
+      './Portal.TypeScript/bower_components'
+    ],
+    bowerPaths: [
+      './Portal.TypeScript/bower.json'
+    ]
+};
+
+// Gulp task to remove all bower_components directories
+gulp.task('bower-clean', function (cb) {
+    return $.del(config.bowerInstallPaths, cb);
+});
+
+// Gulp task to install bower packages
+gulp.task('bower-install', function () {
+    return bower();
+});
+
+gulp.task("default", ["bower-clean", "bower-install"]);
